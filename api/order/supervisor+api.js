10b294a0c12d61e6d34a7e24b906eb16217303f8
DevAmer98 <devamer98@gmail.com>
2025-05-25 15:15:30 +0300

    hhh

--- api/order/supervisor+api.js
+++ api/order/supervisor+api.js
@@ -375,6 +375,7 @@ router.get('/orders/supervisor', async (req, res) => {
         clients.street AS client_street,
         clients.city AS client_city,
         clients.region AS client_region, 
+        clients.username_add AS client_username_add  
         orders.status,
         orders.storekeeperaccept,
         orders.actual_delivery_date,
