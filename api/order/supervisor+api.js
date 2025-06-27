8413ee669b19a3c1e3cdea55638a30d5e2d21845
DevAmer98 <devamer98@gmail.com>
2025-06-28 00:31:09 +0300

    ttt

--- api/order/supervisor+api.js
+++ api/order/supervisor+api.js
@@ -160,7 +160,7 @@ const newOrderNumber = maxOrderNumber + 1;
         const orderResult = await withTimeout(
           client.query(
             `INSERT INTO orders (client_id, username, delivery_date, delivery_type, notes, total_vat, total_subtotal, status, custom_id,order_number)
-             VALUES ($1, $2, $3, $4, $5, $6, $7,$8, $9.$10) RETURNING id`,
+             VALUES ($1, $2, $3, $4, $5, $6, $7,$8, $9,$10) RETURNING id`,
             [client_id, username, formattedDate, delivery_type, notes || null,total_vat, total_subtotal, status, customId,newOrderNumber]
           ),
           10000
