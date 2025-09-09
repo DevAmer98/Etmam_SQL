526def06ad483a57403bc45f3c43b6019e2ea8b1
DevAmer98 <devamer98@gmail.com>
2025-09-09 15:22:05 +0300

    ssl for accountant

--- api/accountant/accountant+api.js
+++ api/accountant/accountant+api.js
@@ -8,7 +8,6 @@ const router = Router();
 // PostgreSQL pool setup
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
